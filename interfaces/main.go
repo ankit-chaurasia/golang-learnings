2ea72195cb6c1ba63e70a669cd33b9532f12473a
Ankit Chaurasia <Ankit.Chaurasia@walmart.com>
2024-02-19 23:25:44 +0530

    Http pakage

---
+++ interfaces/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+type bot interface {
+	getGreeting() string
+}
+
+type englishBot struct{}
+
+type spanishBot struct{}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+func (eb englishBot) getGreeting() string {
+	// very custom login for generating english greetings
+	return "Hi There!"
+}
+
+func (sb spanishBot) getGreeting() string {
+	// very custom login for generating english greetings
+	return "Hola!"
+}
